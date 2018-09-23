f63a6e6aa171cfebdbfac11bb423b3480cf8e48a
vektorel <vektorel@lab2-9>
2018-09-23 18:45:24 +0300

    index ekleme

--- src/com/vektorel/models/tbldefault.java
+++ src/com/vektorel/models/tbldefault.java
@@ -1,5 +1,29 @@
 package com.vektorel.models;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Table
+@Entity
 public class tbldefault {
+	
+	private long id ;
+	private String ad ;
+	
+	
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getAd() {
+		return ad;
+	}
+	public void setAd(String ad) {
+		this.ad = ad;
+	}
+	
 
 }
